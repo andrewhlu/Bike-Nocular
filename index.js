a5a29e2d45417eeba5738d22c463b6816dc9c852
andrewhlu <alu@ucsb.edu>
2018-03-14 16:32:13 -0700

    Add bike selection dropdown

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ function initMap() {
     map: map
   });
 }
+
+function refreshmap() {
+  var bike = document.getElementById('bikelist').value;
+  if(bike == 1) {
+    var uluru = {lat: 34.416700, lng: -119.845230};
+  }
+  else {
+    var uluru = {lat: 34.412900, lng: -119.845796};
+  }
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 20,
+    center: uluru,
+    mapTypeId: 'satellite'
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
